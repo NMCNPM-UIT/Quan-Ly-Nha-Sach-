578070af52eb6d2fafebcf6c1614941aadc6fc6b
huutri148 <huutri148@gmail.com>
2020-06-10 11:54:59 +0700

    Error lose many file

--- BookStoreClone/ViewModel/HoaDonViewModel.cs
+++ BookStoreClone/ViewModel/HoaDonViewModel.cs
@@ -1,18 +1,55 @@
-﻿using System;
+﻿using BookStoreClone.Model;
+using BookStoreClone.View;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Navigation;
 
 namespace BookStoreClone.ViewModel
 {
     class HoaDonViewModel : BaseViewModel
     {
-        public ICommand LoadedThongTin { get; set; }
-        public HoaDonViewModel ()
+        public ICommand LoadedKHCommand;
+        public ICommand LoadedSachCommand;
+        public ICommand SelectionChangedSachCommand;
+        private ObservableCollection<CTHD> _listCTHD;
+        public ObservableCollection<CTHD> ListCTHD { get => _listCTHD; set => _listCTHD = value; }
+        public HoaDonViewModel()
         {
 
+            LoadedKHCommand = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
+            {
+                p.ItemsSource = getKH();
+            });
+            LoadedSachCommand = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
+            {
+                p.ItemsSource = getSach();
+            });
+            SelectionChangedSachCommand = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
+            {
+                p.ItemsSource = getSach();
+            });
+
+
         }
+        private ObservableCollection<KhachHang> getKH()
+        {
+            return new ObservableCollection<KhachHang>(DataProvider.Ins.DB.KhachHangs);
+        }
+        private ObservableCollection<Sach> getSach()
+        {
+            return new ObservableCollection<Sach>(DataProvider.Ins.DB.Saches);
+        }
+        private ObservableCollection<CTHD> getCTHD()
+        {
+
+            return ListCTHD;
+        }
+
     }
 }
