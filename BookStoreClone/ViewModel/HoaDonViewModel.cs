93f18f36c8d36b3382de7e499c18eeb49d2beac4
huutri148 <huutri148@gmail.com>
2020-06-10 16:26:15 +0700

    HoaDon ==> MVVM

--- BookStoreClone/ViewModel/HoaDonViewModel.cs
+++ BookStoreClone/ViewModel/HoaDonViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Navigation;
@@ -14,25 +15,61 @@ namespace BookStoreClone.ViewModel
 {
     class HoaDonViewModel : BaseViewModel
     {
-        public ICommand LoadedKHCommand;
-        public ICommand LoadedSachCommand;
+        public bool IsLoaded = false;
         public ICommand SelectionChangedSachCommand;
         private ObservableCollection<CTHD> _listCTHD;
-        public ObservableCollection<CTHD> ListCTHD { get => _listCTHD; set => _listCTHD = value; }
+        private int _soLuongConLai;
+        private string _tacGiaSach;
+        private string _theLoaiSach;
+        private double _donGiaBan;
+        private ObservableCollection<KhachHang> _listKH;
+        private ObservableCollection<Sach> _listSach;
+        public ObservableCollection<CTHD> ListCTHD { get => _listCTHD; set { _listCTHD = value; OnPropertyChanged(); } }
+        public int SoLuongConLai { get => _soLuongConLai; set{ _soLuongConLai = value; OnPropertyChanged(); } }
+        public string TheLoaiSach { get => _theLoaiSach; set { _theLoaiSach = value; OnPropertyChanged(); } }
+        public string TacGiaSach { get => _tacGiaSach; set {_tacGiaSach = value; OnPropertyChanged(); } }
+        public double DonGiaBan { get => _donGiaBan; set { _donGiaBan = value; OnPropertyChanged(); } }
+        public ObservableCollection<KhachHang> ListKH { get => _listKH; set { _listKH = value; OnPropertyChanged(); } }
+        public ObservableCollection<Sach> ListSach { get => _listSach; set {_listSach = value; OnPropertyChanged(); } }
+
         public HoaDonViewModel()
         {
-
-            LoadedKHCommand = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
-            {
-                p.ItemsSource = getKH();
-            });
-            LoadedSachCommand = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
-            {
-                p.ItemsSource = getSach();
-            });
+            ListCTHD = new ObservableCollection<CTHD>();
+            ListSach = getSach();
+            ListKH = getKH();
             SelectionChangedSachCommand = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
             {
-                p.ItemsSource = getSach();
+                Sach a = (p as ComboBox).SelectedItem as Sach;
+                SoLuongConLai = (int)a.SoLuongTon;
+                DonGiaBan = (double)a.DonGia *(double) 1.15;
+                int i = 0;
+                foreach (var b in a.TacGias)
+                {
+                    if(i != a.TacGias.Count)
+                    {
+                        TacGiaSach += b.TenTG + ", ";
+                        i++;
+                    }    
+                    else
+                    {
+                        i = 0;
+                        TacGiaSach += b.TenTG;
+                    }    
+                    
+                }
+                foreach (var b in a.TheLoais)
+                {
+                    if (i != a.TheLoais.Count)
+                    {
+                        TheLoaiSach += b.TenTL + ", ";
+                        i++;
+                    }
+                    else
+                    {
+                        i = 0;
+                        TheLoaiSach += b.TenTL;
+                    }
+                }
             });
 
 
